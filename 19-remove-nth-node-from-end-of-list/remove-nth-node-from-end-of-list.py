b428440cdfffce20d11992fef7b7653b206e0e5a
Sai Kundan Suddapalli <46628243+Ironman20121@users.noreply.github.com>
2025-01-07 16:49:02 -0500

    Time: 3 ms (6.33%) | Memory: 17.7 MB (29.42%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,55 +5,20 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # reversing ll and deleting ele and reversing it again
-        curr = head
-        perv = None
-        
-        while curr:
-            temp = curr.next
-            curr.next = perv
-            perv = curr
-            curr = temp
-        
-        curr = perv
-        
-        if n==1:
-            perv = perv.next
-        else :
-            count = n-2
-            while count:
-                count = count -1
-                curr = curr.next
-            curr.next = curr.next.next
-        
-        curr =  perv
+        dummy = ListNode(0,head)
+        left = dummy
+        right = head
 
-        while curr:
-            print(curr.val,end="->")
-            curr = curr.next
-       
-        
-        curr = perv
-        perv2 = None
-        temp = None 
+        while n > 0 :
+            right = right.next
+            n-=1
 
-        while curr:
-            temp  = curr.next
-            curr.next = perv2
-            perv2 = curr
-            curr = temp
-        
-        curr = perv2
-        print("end")
+        while right:
+            left = left.next
+            right = right.next
 
-        # while curr:
-          
-        #     print(curr.val,end="->")
-        #     curr = curr.next
-
-        return perv2
-
-        
+        left.next = left.next.next
+        return dummy.next        
 
 
         
