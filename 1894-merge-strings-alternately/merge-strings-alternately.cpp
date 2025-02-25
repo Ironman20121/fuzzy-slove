6d5d69af2cfa4d2d2db21c4dc01d101d49e7ba43
Sai Kundan Suddapalli <46628243+Ironman20121@users.noreply.github.com>
2025-02-25 11:49:48 -0500

    Time: 4 ms (30.44%) | Memory: 8.5 MB (63.08%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string min_word, max_word,merge="";
-        if (word1.length() <= word2.length()){
-            min_word = word1;
-            max_word = word2;
+        // string min_word, max_word,merge="";
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int i =0,j =0;
+        string merge = "";
+        while(i<len1 && j<len2){
+            merge+=word1[i];
+            merge+=word2[j];
+            i++;
+            j++;
         }
-        else{
-            min_word = word2;
-            max_word = word1;
+        while(i<len1){
+            merge+=word1[i];
+            i++;
         }
-        for(int i=0;i<min_word.length();i++)
-        {  merge =  merge + word1[i] + word2[i]; }
-        
-        if (word1.length()==word2.length()){return merge;}
+        while(j<len2){
+            merge+=word2[j];
+            j++;
+        }
 
-        else{
-            for(int i=min_word.length();i<max_word.length();i++){
-                merge = merge+ max_word[i];
-            }
-        }
+
+        // if (word1.length() <= word2.length()){
+        //     min_word = word1;
+        //     max_word = word2;
+        // }
+        // else{
+        //     min_word = word2;
+        //     max_word = word1;
+        // }
+        // for(int i=0;i<min_word.length();i++)
+        // {  merge =  merge + word1[i] + word2[i]; }
+        
+        // if (word1.length()==word2.length()){return merge;}
+
+        // else{
+        //     for(int i=min_word.length();i<max_word.length();i++){
+        //         merge = merge+ max_word[i];
+        //     }
+        // }
         return merge;
     
         
