160f7558e8262328edb2e1f9cb93fda77ed23248
Sai Kundan Suddapalli <46628243+Ironman20121@users.noreply.github.com>
2025-03-04 14:21:47 -0500

    Time: 0 ms (100.00%) | Memory: 13.9 MB (19.91%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -5,20 +5,19 @@ public:
         vector<vector<int>>res;
         vector<int>subset;
         function<void(vector<int>&,int )>bt=[&](auto &subset,int i){
-            //falure base case and succues base case
-            //handling out of bonce
             if(i>=nums.size()){
-                for(auto &j:res){
-                    if(j==subset){
-                        return;
-                    }
-                }
+                // for(auto &j:res){
+                //     if(j==subset){
+                //         return;
+                //     }
+                // }
                 res.push_back(subset);
                 return;
             }
             subset.push_back(nums[i]);
             bt(subset,i+1);
             subset.pop_back();
+            while(i+1<nums.size()&& nums[i]==nums[i+1]){i++;}
             bt(subset,i+1);
         };
         bt(subset,0);
