f101a023f51ae4edafd1e024e43e2460e5db9f98
Sai Kundan Suddapalli <46628243+Ironman20121@users.noreply.github.com>
2025-03-05 14:55:24 -0500

    Time: 34 ms (23.30%) | Memory: 23 MB (34.89%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int ROWS = grid.size();
+        if (ROWS ==0)return 0;
+        int COLS = grid[0].size();
+        int islands=0;
+
+        function<void(int ,int)> bfs = [&](int row,int col){
+            int directions[4][2]  = {{1,0},{-1,0},{0,1},{0,-1}};
+            queue<pair<int,int>> q ;
+            q.push({row,col});
+            grid[row][col]='0';
+
+            while(!q.empty()){
+                auto node = q.front(); 
+                q.pop();
+                int r = node.first;
+                int c = node.second;
+                for(int i=0;i<4;i++){
+                    int nr = r + directions[i][0];
+                    int nc = c + directions[i][1];
+                    if(nr >=0 && nc >=0 && nr <grid.size() && nc <grid[0].size() &&grid[nr][nc]=='1' ){
+                        q.push({nr,nc});
+                        grid[nr][nc]='0';
+                    }
+
+                }   
+            }
+        };
+
+        for(int r=0;r<ROWS;r++){
+            for(int c=0;c<COLS;c++){
+                if(grid[r][c]=='1'){
+                    bfs(r,c);
+                    islands++;
+                }
+            }
+        }
+
+        return islands;
+        
+    }
+};
