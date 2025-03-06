6b4b8cf0a31f96979356108177875f1adf4745e0
Sai Kundan Suddapalli <46628243+Ironman20121@users.noreply.github.com>
2025-03-06 07:40:36 -0500

    Time: 0 ms (100.00%) | Memory: 26.5 MB (88.31%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> countrack(n*n);
+
+        for(int row=0;row<grid.size();row++){
+            for(int col = 0;col<grid[0].size();col++){
+                countrack[grid[row][col]-1]++;
+            }
+        }
+        int twice_num=0,missing_num=0;
+        for(int i=0;i<n*n;i++){
+            if(countrack[i]==2){twice_num = i+1;}
+            if(countrack[i]==0){missing_num=i+1;}
+            
+        }
+        
+       return {twice_num,missing_num};
+    }
+};
