fafb2e59c7bb052081ace2c30f1daa55068ecb5b
Sai Kundan Suddapalli <46628243+Ironman20121@users.noreply.github.com>
2025-03-11 21:01:19 -0400

    Time: 0 ms (100.00%) | Memory: 17.2 MB (5.27%) - LeetSync

---
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalOrder(TreeNode* root) {
+        map<int,map<int,vector<int>>>order;
+        function<void(TreeNode*,int,int)>dfs =[&](TreeNode*root,int index,int row){
+                if (!root)return;
+                 order[index][row].push_back(root->val);
+                 dfs(root->left,index-1,row+1);
+                 dfs(root->right,index+1,row+1);      
+        };
+        dfs(root,0,0);
+        vector<vector<int>>ans;
+        for (auto& colPair : order) {
+        vector<int> colVals;
+        for (auto& rowPair : colPair.second) {
+        colVals.insert(colVals.end(), rowPair.second.begin(), rowPair.second.end());
+        }
+            ans.push_back(colVals);
+        }
+
+        return ans;
+    }
+};
