5288331d2237ffd00a2143711df77c6fbc850e87
Sai Kundan Suddapalli <46628243+Ironman20121@users.noreply.github.com>
2025-03-13 00:22:20 -0400

    Time: 8 ms (9.03%) | Memory: 28.6 MB (6.13%) - LeetSync

---
+++ 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> closestKValues(TreeNode* root, double target, int k) {
+        vector<int>ans;
+        map<double,vector<int>>t;
+        function<void(TreeNode*)>dfs =[&](TreeNode*root){
+            if (!root)return;
+                t[abs(root->val - target)].push_back(root->val);
+                // t[abs(root->val - target)].insert(t[abs(root->val -target)].end(),root->val);
+                dfs(root->left);
+                dfs(root->right);
+        };
+        
+        dfs(root);
+        for(auto&i:t){
+            if (k==0)break;
+            for(auto j:i.second){
+                ans.insert(ans.end(),j);
+                k--;
+            }   
+        }
+        return ans;
+    }
+};
